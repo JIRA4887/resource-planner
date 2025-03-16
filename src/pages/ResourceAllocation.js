2186cfe2271bbabca0419a934a2cdd0e523393c5
JIRA4887 <57528501+JIRA4887@users.noreply.github.com>
2025-03-16 20:13:35 +0530

    Create ResourceAllocation.js

---
+++ src/pages/ResourceAllocation.js
@@ -0,0 +1,14 @@
+import SkillAllocation from '../components/SkillAllocation';
+import ExcelImport from '../components/ExcelImport';
+
+const ResourceAllocation = () => {
+  return (
+    <div>
+      <h1>Resource Allocation</h1>
+      <SkillAllocation />
+      <ExcelImport />
+    </div>
+  );
+};
+
+export default ResourceAllocation;
