7c732cd60736d9e3b80b46008afdcf771a60b548
JIRA4887 <57528501+JIRA4887@users.noreply.github.com>
2025-03-16 20:54:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import NodePolyfills from '@esbuild-plugins/node-polyfill';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'global': {},
-    'process.env': {},
-    'crypto': 'require("crypto")'
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        NodePolyfills({
+          crypto: true,
+        })
+      ]
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 });
