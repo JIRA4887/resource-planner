130b40e384ff8dbd8dbcf9ec95c919df34ae889f
JIRA4887 <57528501+JIRA4887@users.noreply.github.com>
2025-03-16 20:14:19 +0530

    Create Chat.js

---
+++ src/pages/Chat.js
@@ -0,0 +1,12 @@
+import ChatRoom from '../components/ChatRoom';
+
+const Chat = () => {
+  return (
+    <div>
+      <h1>Real-Time Collaboration</h1>
+      <ChatRoom />
+    </div>
+  );
+};
+
+export default Chat;
