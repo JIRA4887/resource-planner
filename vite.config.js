25f32769ee204b48b21212cd9060e9d2e48fd27e
JIRA4887 <57528501+JIRA4887@users.noreply.github.com>
2025-03-16 20:40:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import GlobalsPolyfills from '@esbuild-plugins/node-globals-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        GlobalsPolyfills({
+          buffer: true,
+          process: true,
+        }),
+      ],
+    },
+  },
   build: {
-    target: 'esnext',  // Support latest JS features
-    outDir: 'dist',    // Output folder
+    target: 'esnext', 
+    outDir: 'dist',
   },
 });
