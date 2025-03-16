c6808a378c1574cbf5ae2780740e4586ab50bc60
JIRA4887 <57528501+JIRA4887@users.noreply.github.com>
2025-03-16 20:44:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {},
-    global: 'globalThis'
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true
+        })
+      ]
+    }
   },
   resolve: {
     alias: {
       crypto: 'crypto-browserify'
     }
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      }
-    }
-  },
-  build: {
-    target: 'esnext'
+  define: {
+    'process.env': {}
   }
 });
