479e0403397e51621320f3ba9ce5edc00fd6b5a6
JIRA4887 <57528501+JIRA4887@users.noreply.github.com>
2025-03-16 20:13:58 +0530

    Create Reports.js

---
+++ src/pages/Reports.js
@@ -0,0 +1,12 @@
+import ReportsExport from '../components/ReportsExport';
+
+const Reports = () => {
+  return (
+    <div>
+      <h1>Reports and Analytics</h1>
+      <ReportsExport />
+    </div>
+  );
+};
+
+export default Reports;
